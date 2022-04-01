f21832df494e946b17b85f801c794c3c86120238
Abdul Azis <83083246+Aziszy@users.noreply.github.com>
2022-04-01 20:54:03 +0700

    dapat menjalankan fitur standby
    saat tombol standby di tekan akan menampilkan status team yang dapat
    menekan tombol, dan apabila di tekan sekali lagi akan menampilkan nama
    team beserta scorenya

--- sistem_cerdas_cermat.ino
+++ sistem_cerdas_cermat.ino
@@ -1,73 +1,172 @@
-#define pbt1 11
-#define pbt2 10
-#define pbt3 9
-#define startLedPin 2
-#define endLedPin 8
+#define pbt1 13
+#define pbt2 12
+#define pbt3 11
+#define standByBtnPin A0
 
-int pin = 0;
+#define data1 2
+#define latch1 3
+#define clock1 4
+
+#define data2 5
+#define latch2 6
+#define clock2 7
+
+#define data3 8
+#define latch3 9
+#define clock3 10
+
+int countStandByClick = 0;
+bool isStandBy = false;
 bool isLocked = false;
 
-void setLedOuput(char inputString)
+int shift1[3] = {data1, latch1, clock1};
+int shift2[3] = {data2, latch2, clock2};
+int shift3[3] = {data3, latch3, clock3};
+
+int n = sizeof(shift1) / sizeof(shift1[0]);
+
+void setLedOuput(int *pinData, int pinDataSize, char inputString)
 {
     switch (inputString)
     {
+    case 'E':
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b00000000);
+        digitalWrite(pinData[1], HIGH);
+        break;
     case 'A':
-        for (pin = startLedPin; pin <= endLedPin; pin++)
-        {
-            if (pin != 7)
-                digitalWrite(pin, HIGH);
-            else
-                digitalWrite(pin, LOW);
-        }
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b11111010);
+        digitalWrite(pinData[1], HIGH);
         break;
     case 'B':
-        for (pin = startLedPin; pin <= endLedPin; pin++)
-        {
-            if (pin != 2 && pin != 3)
-                digitalWrite(pin, HIGH);
-            else
-                digitalWrite(pin, LOW);
-        }
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b00111110);
+        digitalWrite(pinData[1], HIGH);
         break;
     case 'C':
-        for (pin = startLedPin; pin <= endLedPin; pin++)
-        {
-            if (pin != 3 && pin != 4 && pin != 8)
-                digitalWrite(pin, HIGH);
-            else
-                digitalWrite(pin, LOW);
-        }
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b10011100);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 0:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b11111100);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 1:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b01100000);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 2:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b11011010);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 3:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b11110010);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 4:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b11110010);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 5:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b10110110);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 6:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b10111110);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 7:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b11100000);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 8:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b11111110);
+        digitalWrite(pinData[1], HIGH);
+        break;
+    case 9:
+        digitalWrite(pinData[1], LOW);
+        shiftOut(pinData[0], pinData[2], LSBFIRST, 0b11110110);
+        digitalWrite(pinData[1], HIGH);
         break;
     }
 }
+
 void setup()
 {
     Serial.begin(9600);
+    // INPUT
     pinMode(pbt1, INPUT_PULLUP);
     pinMode(pbt2, INPUT_PULLUP);
     pinMode(pbt3, INPUT_PULLUP);
+    pinMode(standByBtnPin, INPUT_PULLUP);
 
-    for (pin = startLedPin; pin <= endLedPin; pin++)
-    {
-        pinMode(pin, OUTPUT);
-    }
+    // OUTPUT
+    pinMode(data1, OUTPUT);
+    pinMode(latch1, OUTPUT);
+    pinMode(clock1, OUTPUT);
+
+    pinMode(data2, OUTPUT);
+    pinMode(latch2, OUTPUT);
+    pinMode(clock2, OUTPUT);
+
+    pinMode(data3, OUTPUT);
+    pinMode(latch3, OUTPUT);
+    pinMode(clock3, OUTPUT);
+
+    setLedOuput(shift1, n, 'E');
+    setLedOuput(shift2, n, 'E');
+    setLedOuput(shift3, n, 'E');
 }
 
 void loop()
 {
-    if (isLocked == false)
+    if (digitalRead(standByBtnPin) == LOW)
     {
-        if (digitalRead(pbt1) == LOW)
+        if (countStandByClick < 3)
         {
-            setLedOuput('A');
+            countStandByClick++;
+            delay(500);
         }
-        if (digitalRead(pbt2) == LOW)
+
+        if (countStandByClick == 2)
         {
-            setLedOuput('B');
+            isStandBy = true;
         }
-        if (digitalRead(pbt3) == LOW)
+        else
         {
-            setLedOuput('C');
+            setLedOuput(shift1, n, 'A');
+            setLedOuput(shift2, n, 'B');
+            setLedOuput(shift3, n, 'C');
+            isStandBy = false;
         }
+    }
+
+    if (isStandBy)
+    {
+        setLedOuput(shift1, n, 'A');
+        setLedOuput(shift2, n, 0);
+        setLedOuput(shift3, n, 1);
+        delay(2000);
+
+        setLedOuput(shift1, n, 'B');
+        setLedOuput(shift2, n, 0);
+        setLedOuput(shift3, n, 2);
+        delay(2000);
+
+        setLedOuput(shift1, n, 'C');
+        setLedOuput(shift2, n, 0);
+        setLedOuput(shift3, n, 3);
+        delay(2000);
     }
 }
