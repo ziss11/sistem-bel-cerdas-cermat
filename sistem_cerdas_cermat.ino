55de770d066e106729948079e07a009864698183
Abdul Azis <83083246+Aziszy@users.noreply.github.com>
2022-04-04 17:02:48 +0700

    menambahkan sistem tambah dan kurangi skor

--- sistem_cerdas_cermat.ino
+++ sistem_cerdas_cermat.ino
@@ -1,15 +1,15 @@
-#define pbt1 5
-#define pbt2 6
-#define pbt3 7
-#define standByBtnPin 8
-#define incScorePin 9
-#define decScorePin 10
+#define pbt1 6
+#define pbt2 7
+#define pbt3 8
+#define standByBtnPin 9
+#define incScorePin 10
+#define decScorePin 11
 
-#define interupt 11
+#define interupt 2
 
-#define data 2
-#define latch 3
-#define clock 4
+#define data 3
+#define latch 4
+#define clock 5
 
 volatile boolean ledAisOn = false;
 volatile boolean ledBisOn = false;
@@ -18,6 +18,7 @@ volatile boolean ledCisOn = false;
 int countStandByClick = 0;
 bool isStandBy = true;
 bool isLocked = false;
+bool isAnswered = false;
 
 char currentTeamStand = 'A';
 
@@ -25,7 +26,7 @@ int scoreTeamA = 0;
 int scoreTeamB = 0;
 int scoreTeamC = 0;
 
-int digit[14]{
+int digit[16]{
     0b11111100, // 0
     0b01100000, // 1
     0b11011010, // 2
@@ -40,6 +41,8 @@ int digit[14]{
     0b00111110, // b
     0b10011100, // c
     0b00000000, // e
+    0b01101110, // X salah
+    0b00111010, // o benar
 };
 
 void displayOutput(int digit1, int digit2, int digit3)
@@ -65,6 +68,10 @@ void standByBtnAction()
         isStandBy = true;
         displayOutput(10, 11, 12);
     }
+    isLocked = false;
+    ledAisOn = false;
+    ledBisOn = false;
+    ledCisOn = false;
 }
 
 void participantAction()
@@ -73,6 +80,7 @@ void participantAction()
     {
         if (digitalRead(pbt1) == LOW)
         {
+            currentTeamStand = 'A';
             if (ledAisOn)
             {
                 ledAisOn = false;
@@ -84,9 +92,11 @@ void participantAction()
                 displayOutput(10, 13, 13);
             }
             isLocked = true;
+            isAnswered = true;
         }
         else if (digitalRead(pbt2) == LOW)
         {
+            currentTeamStand = 'B';
             if (ledBisOn)
             {
                 ledBisOn = false;
@@ -98,9 +108,11 @@ void participantAction()
                 displayOutput(13, 11, 13);
             }
             isLocked = true;
+            isAnswered = true;
         }
         else if (digitalRead(pbt3) == LOW)
         {
+            currentTeamStand = 'C';
             if (ledCisOn)
             {
                 ledCisOn = false;
@@ -112,6 +124,7 @@ void participantAction()
                 displayOutput(13, 13, 12);
             }
             isLocked = true;
+            isAnswered = true;
         }
     }
 }
@@ -120,13 +133,13 @@ void scoreBoardAction()
 {
     if (isStandBy == false)
     {
-        displayOutput(10, scoreTeamA, scoreTeamA);
+        displayOutput(10, floor(scoreTeamA / 10), scoreTeamA % 10);
         delay(2000);
 
-        displayOutput(11, scoreTeamB, scoreTeamB);
+        displayOutput(11, floor(scoreTeamB / 10), scoreTeamB % 10);
         delay(2000);
 
-        displayOutput(12, scoreTeamC, scoreTeamC);
+        displayOutput(12, floor(scoreTeamC / 10), scoreTeamC % 10);
         delay(2000);
 
         countStandByClick = 0;
@@ -136,36 +149,57 @@ void scoreBoardAction()
 
 void scoreMechanism()
 {
-    if (digitalRead(incScorePin) == LOW)
+    if (isAnswered)
     {
-        if (currentTeamStand == 'A')
+        if (digitalRead(incScorePin) == LOW)
         {
-            scoreTeamA++;
+            if (currentTeamStand == 'A')
+            {
+                scoreTeamA += 2;
+            }
+            else if (currentTeamStand == 'B')
+            {
+                scoreTeamB += 2;
+            }
+            else if (currentTeamStand == 'C')
+            {
+                scoreTeamC += 2;
+            }
+            displayOutput(15, 15, 15);
+            isAnswered = false;
         }
-        else if (currentTeamStand == 'B')
+        else if (digitalRead(decScorePin) == LOW)
         {
-            scoreTeamB++;
-        }
-        else if (currentTeamStand == 'C')
-        {
-            scoreTeamC++;
+            if (currentTeamStand == 'A')
+            {
+                scoreTeamA--;
+                scoreTeamA = checkScore(scoreTeamA);
+            }
+            else if (currentTeamStand == 'B')
+            {
+                scoreTeamB--;
+                scoreTeamB = checkScore(scoreTeamB);
+            }
+            else if (currentTeamStand == 'C')
+            {
+                scoreTeamC--;
+                scoreTeamC = checkScore(scoreTeamC);
+            }
+            displayOutput(14, 14, 14);
+            isAnswered = false;
+            isLocked = false;
+            participantAction();
         }
     }
-    else if (digitalRead(decScorePin) == LOW)
+}
+
+int checkScore(int score)
+{
+    if (score < 0)
     {
-        if (currentTeamStand == 'A')
-        {
-            scoreTeamA--;
-        }
-        else if (currentTeamStand == 'B')
-        {
-            scoreTeamB--;
-        }
-        else if (currentTeamStand == 'C')
-        {
-            scoreTeamC--;
-        }
+        score = 0;
     }
+    return score;
 }
 
 void setup()
@@ -176,8 +210,10 @@ void setup()
     pinMode(pbt2, INPUT_PULLUP);
     pinMode(pbt3, INPUT_PULLUP);
     pinMode(standByBtnPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(interupt), participantAction, RISING);
-
+    pinMode(interupt, INPUT);
+    attachInterrupt(digitalPinToInterrupt(interupt), participantAction, LOW);
+    pinMode(incScorePin, INPUT_PULLUP);
+    pinMode(decScorePin, INPUT_PULLUP);
     pinMode(data, OUTPUT);
     pinMode(latch, OUTPUT);
     pinMode(clock, OUTPUT);
@@ -192,6 +228,6 @@ void loop()
         standByBtnAction();
     }
     scoreBoardAction();
-    participantAction();
+    // participantAction();
     scoreMechanism();
 }
